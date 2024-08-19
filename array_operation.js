cb7a783c13ee456bdf8072c22688993e43a9f713
habib-technonext <habibur.rahman@technonext.com>
2024-08-19 12:50:43 +0600

    array operation initialized

---
+++ array_operation.js
@@ -0,0 +1,6 @@
+let arr = [1, 2, 3];
+// push operation
+arr.push(4); // arr is now [1, 2, 3, 4] - add element at the end of array
+// pop operation
+arr.pop(); // arr is now [1, 2, 3] - remove the last element from the array
+console.log(arr);
